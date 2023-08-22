fee26f97d3fbb05738d47e2a8e6dccb4fff470cc
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-08-22 14:28:19 +0530

    add test case to check configuration part is ok

---
+++ src/main/java/org/hostelManagement/entitiy/Student.java
@@ -0,0 +1,30 @@
+package org.hostelManagement.entitiy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    private String studentID;
+
+    private String studentName;
+
+    private String studentAddress;
+
+    private String studentContact;
+
+    private String studentDOB;
+
+    private String studentGender;
+
+}
